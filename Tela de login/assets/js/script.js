01f7ee30a811e229d004a5bbf85a3515a60affcc
Everton Solon Tiba <evertonsolon@outlook.com>
2022-06-27 02:13:38 -0300

    Trabalhando com JSON

--- Tela de login/assets/js/script.js
+++ Tela de login/assets/js/script.js
@@ -1,26 +1,77 @@
-let email = 'teste@teste.com.br';
-console.log(email)
+// let email = 'teste@teste.com.br';
+// console.log(email)
 
-console.log('O e-mail é: ' + email)
-console.log(`O e-mail é: ${email} <- utilizando template string`)
+// console.log('O e-mail é: ' + email)
+// console.log(`O e-mail é: ${email} <- utilizando template string`)
 
 document.getElementById('btn-submit').addEventListener('click', e => {
 
-    console.log('O botão foi clicado!');
+    //console.log('O botão foi clicado!');
 });
 
 document.getElementById('form-login').addEventListener('mouseenter', e => {
 
-    console.log('O mouse está sobre o formulário!');
+    // console.log('O mouse está sobre o formulário!');
+
+
 });
 
 document.querySelector('#form-login').addEventListener('mouseleave', e => {
 
-    console.log('O mouse está fora do formulário!');
+    //console.log('O mouse está fora do formulário!');
 });
 
 document.querySelector('#form-login').addEventListener('submit', e => {
 
-    e.preventDefault();
-    console.log('Formulário enviado!');
+    // e.preventDefault();
+    
+    // let email = document.querySelector('#email').value;
+    // let password = document.querySelector('#password').value;
+    
+    // console.log(email, password);
+
+
+});
+
+
+document.getElementById('form-login').addEventListener('mouseleave', e => {
+
+    //Para testes diversos
+    // let email = document.querySelector('#email');
+    // let password = document.querySelector('#password');
+
+    // console.log(email);
+    // console.log(password);
+
+
+    //let var_email = document.querySelector('#email').value;
+    //let var_password = document.querySelector('#password').value;
+
+    // let json = 
+    // {
+    //     email: var_email
+    // };
+
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    // Quando o nome da variável é o mesmo da propriedade do Json, basta informar apenas uma única vez.
+    let json = 
+        {
+            email,
+            password
+
+        };
+
+    //console.log(json);
+    //console.log(json.email);
+
+    let stringJson = JSON.stringify(json);
+
+    //console.log(stringJson);
+
+    let jsonParse = JSON.parse(stringJson);
+
+    console.log(jsonParse);
+
 });
